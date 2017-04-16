669693af058943812a5cbfa0843e00c3f1a1f501
Nuno Tavares <Nuno@iMac.local>
2017-04-15 21:23:53 -0400

    second commit

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -49,13 +49,50 @@ class MainForm extends Component {
          otherMedical2:'',
          otherMedical3:''
        },
+       insurance: {
+         lifeInsurance:'',
+         healthInsurance:'',
+         autoInsurance:'',
+         homeRentalInsurance:'',
+         disabilityInsurance:'',
+         longTermInsurance:''
+       },
+       transportation: {
+         carGas:'',
+         carRepairs:'',
+         bus:'',
+         train:'',
+         newCarFund:'',
+         othertransportation:''
+       },
+        personal: {
+         educationTution:'',
+         childCare:'',
+         toiletries:'',
+         hairCareCosmetics:'',
+         bookSupplies:'',
+         childSupport:'',
+         alimony:'',
+         pocketMoney:'',
+         subscriptions:'',
+         babySupplies:'',
+         petSupplies:'',
+         miscellaneous: '',
+         otherPersonal:'',
+         otherPersonal1:'',
+         otherPersonal2:''
+
+       },
         section: "pay",
         section: "food",
         section: "clothing",
         section: "utilities",
         section: "housing",
         section: "savings",
-        section: "medical"
+        section: "medical",
+        section: "insurance",
+        section: "transportation",
+        section: "personal"
     };
 
     // binding our methods which get passed as callbacks
@@ -266,6 +303,149 @@ class MainForm extends Component {
     );
   }
 
+  renderInsurance() {
+    return (
+      <div>
+        <h1> Insurance </h1>
+          <br />
+          <label>Life Insurance:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "insurance", "lifeInsurance") } value={this.state.insurance.lifeInsurance}  />
+          <br />
+          <br />
+          <label>Health Insurance:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "insurance", "healthInsurance") } value={this.state.insurance.healthInsurance}  />
+          <br />
+          <br />
+          <label>Auto Insurance:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "insurance", "autoInsurance") } value={this.state.insurance.autoInsurance}  />
+          <br />
+          <br />
+          <label>Home or Rental Insurance:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "insurance", "homeRentalInsurance") } value={this.state.insurance.homeRentalInsurance}  />
+          <br />
+          <br />
+          <label>Disability Insurance:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "insurance", "disabilityInsurance") } value={this.state.insurance.disabilityInsurance}  />
+          <br />
+          <br />
+          <label>Long Term Insurance:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "insurance", "longTermInsurance") } value={this.state.insurance.longTermInsurance}  />
+          <br />
+          <br />
+          <button onClick={() => this.setState({section: "medical"})}>Back </button>
+          <button onClick={() => this.setState({section: "transportation"})}>Next</button>
+      </div>
+    );
+  }
+
+  renderTransportation() {
+    return (
+      <div>
+        <h1> Transportation </h1>
+          <br />
+          <label>Gas for Car:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "transportation", "carGas") } value={this.state.transportation.carGas}  />
+          <br />
+          <br />
+          <label>Car Repairs:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "transportation", "carRepairs") } value={this.state.transportation.carRepairs}  />
+          <br />
+          <br />
+          <label>Bus:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "transportation", "bus") } value={this.state.transportation.bus}  />
+          <br />
+          <br />
+          <label>Train:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "transportation", "train") } value={this.state.transportation.train}  />
+          <br />
+          <br />
+          <label>New Car Fund:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "transportation", "newCarFund") } value={this.state.transportation.newCarFund}  />
+          <br />
+          <br />
+          <label>Other Transportation:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "transportation", "othertransportation") } value={this.state.transportation.othertransportation}  />
+          <br />
+          <br />
+          <button onClick={() => this.setState({section: "insurance"})}>Back </button>
+          <button onClick={() => this.setState({section: "personal"})}>Next</button>
+      </div>
+    );
+  }
+
+  renderPersonal() {
+    return (
+      <div>
+        <h1> Personal </h1>
+          <br />
+          <label>Tution & Eduction:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "educationTution") } value={this.state.personal.educationTution}  />
+          <br />
+          <br />
+          <label>Child Care:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "childCare") } value={this.state.personal.childCare}  />
+          <br />
+          <br />
+          <label>Toiletries:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "toiletries") } value={this.state.personal.toiletries}  />
+          <br />
+          <br />
+          <label>Hair Care & Cosmetics:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "hairCareCosmetics") } value={this.state.personal.hairCareCosmetics}  />
+          <br />
+          <br />
+          <label>Book Supplies:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "bookSupplies") } value={this.state.personal.bookSupplies}  />
+          <br />
+          <br />
+          <label>Child Support:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "childSupport") } value={this.state.personal.childSupport}  />
+          <br />
+          <br />
+          <label>Alimony:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "alimony") } value={this.state.personal.alimony}  />
+          <br />
+          <br />
+          <label>Pocket Money:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "pocketMoney") } value={this.state.personal.pocketMoney}  />
+          <br />
+          <br />
+          <label>Subscriptions:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "subscriptions") } value={this.state.personal.subscriptions}  />
+          <br />
+          <br />
+          <label>Baby Supplies:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "babySupplies") } value={this.state.personal.babySupplies}  />
+          <br />
+          <br />
+          <label>Pet Supplies:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "petSupplies") } value={this.state.personal.petSupplies}  />
+          <br />
+          <br />
+          <label>Miscellaneous:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "miscellaneous") } value={this.state.personal.miscellaneous}  />
+          <br />
+          <br />
+          <label>Other Personal:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "otherPersonal") } value={this.state.personal.otherPersonal}  />
+          <br />
+          <br />
+          <label>Other Personal:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "otherPersonal1") } value={this.state.personal.otherPersonal1}  />
+          <br />
+          <br />
+          <label>Other Personal:</label>
+          <Input onChange={(event) => this.handleInputChange(event, "personal", "otherPersonal2") } value={this.state.personal.otherPersonal2}  />
+          <br />
+          <br />
+          <button onClick={() => this.setState({section: "transportation"})}>Back </button>
+          <button onClick={() => this.setState({section: "entertainment"})}>Next</button>
+      </div>
+    );
+  }
+
+
+
   renderForm() {
     if (this.state.section === "pay") {
       return this.renderPay();
@@ -287,6 +467,15 @@ class MainForm extends Component {
     }
     else if(this.state.section === "medical"){
       return this.renderMedical();
+    }
+    else if(this.state.section === "insurance"){
+      return this.renderInsurance();
+    }
+    else if(this.state.section === "transportation"){
+      return this.renderTransportation();
+    }
+    else if(this.state.section === "personal"){
+      return this.renderPersonal();
     }
   }
 
