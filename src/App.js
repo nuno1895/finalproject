1f2efd09f6d840bfcf732e13c43d0e283c1ffa8e
Steven Gissubel <steven.gissubel@gmail.com>
2017-04-18 12:32:48 -0400

    Revert "Added NavBar"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MainForm from './components/MainForm';
-import Results from './components/Results';
 
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
     return (
       <div className="App">
         <MainForm />
-        {/*<Results />*/}
       </div>
     );
   }
