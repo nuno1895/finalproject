c881a971774a5d5115c3b024870e27a31ff591ec
Nuno Tavares <Nuno@iMac.local>
2017-04-18 20:23:49 -0400

    advice template

---
+++ src/components/Advice.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+{/*
+
+Based on grade and catergory, this will give advice on that to do
+
+*/}
+
+function Advice(props) {
+
+	var Advice = {
+	  food: {
+        a : ["Great Job Your a RockStar"],
+        b : ["Coupons"],
+        c : ["Shoping List", "Don't Eat Out"],
+        d : ["Seriously Don't Eat Out"],
+        f : ["What the Fuck!!! What are you Eating"]
+      },
+       clothing: {
+	    a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       utilities: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       housing: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       savings: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       medical: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       insurance: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       transportation: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       personal: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       entertainment: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+       debt: {
+        a : ["Great Job Your a RockStar"],
+	    b : ["Coupons"],
+	    c : ["Shoping List", "Don't Eat Out"],
+	    d : ["Seriously Don't Eat Out"],
+	    f : ["What the Fuck!!! What are you Eating"]
+       },
+	}
+
+}
+
+export default Advice;
