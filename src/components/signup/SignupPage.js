e8027d7c6fffd768573f055295745f2ab8c5033a
Steven Gissubel <steven.gissubel@gmail.com>
2017-04-21 13:52:27 -0400

    singup page start

---
+++ src/components/signup/SignupPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SignupPage extends React.Component {
+	render() {
+		return (
+			<h1>Sign Up Bitches!</h1>
+		);
+	}
+}
+ export default SignupPage;
